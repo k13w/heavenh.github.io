4c5e70696755f74025a83a06d885e68a594bda0a
Heaven <gilmarheavensilva@hotmail.com>
2016-07-31 17:13:59 -0300

    :p
    :p

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function($) { 
-  $(".info a").click(function(event){        
+  $(".info a, #whoami a").click(function(event){        
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
  });
