1c30d2902f0f7b83634732a17f740ad53d316e67
Heaven <gilmarheavensilva@hotmail.com>
2017-10-02 18:53:07 -0300

    :hammer:
    :hammer:

---
+++ assets/js/scroll.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) { 
+  $(".info a, #whoami a").click(function(event){        
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1200);
+ });
+});
+
+$(document).ready(function() {
+  $('html').niceScroll({
+    cursorborder: '#d1ff83',
+    cursorcolor:  '#d1ff83'
+  });
+});
